61f6597b8f4a94d994408bdd46182e2b3c05f575
Ramon Souza <ramon.souza@objective.com.br>
2022-07-08 10:35:32 -0300

    ci: deploy 08-07-22

---
+++ precache-manifest.b857f937f4d8b3ac1bd07b6a68c1ba10.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2ea373fb2fc766e1920",
+    "url": "/css/app.04ddede9.css"
+  },
+  {
+    "revision": "d2ea373fb2fc766e1920",
+    "url": "/js/app.e3df1b15.js"
+  },
+  {
+    "revision": "dece9d9e75d4af1fe47d",
+    "url": "/css/chunk-vendors.974040d7.css"
+  },
+  {
+    "revision": "dece9d9e75d4af1fe47d",
+    "url": "/js/chunk-vendors.496c98c0.js"
+  },
+  {
+    "revision": "8f90a376d75f29c7d13b85fdeda1ea51",
+    "url": "/img/gota.8f90a376.svg"
+  },
+  {
+    "revision": "d72b6972da68049454852e25c405a209",
+    "url": "/img/2.d72b6972.svg"
+  },
+  {
+    "revision": "cac6e0546c6ddae6ae34d59cca941425",
+    "url": "/img/1.cac6e054.svg"
+  },
+  {
+    "revision": "fcaa8b24e5513ca266ba8d5cab37b7c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
